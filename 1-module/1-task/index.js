4a1528b6aac7a560b2934bee4fc5747bca623eee
Andrey Dorokhov <charizard2991@yandex.ru>
2019-04-29 17:50:29 +0300

    task 1-1 solution

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,31 @@
  * @returns {number}
  */
 function pow (m, n) {
+//    let result = 0;
+    if ( n === 1 ) {
+        return m;
+    } else {
+        return m * pow (m, n-1);
+    }
+}
 
+function checkNum (num) {
+    if (+num >= 1 && isFinite(+num) && Math.ceil(+num) === +num ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let x = prompt ('Enter x');
+let n = prompt ('Enter т');;
+/*let comment = 'Enter n';
+
+do {
+    n = prompt (comment);
+    comment = 'Enter correct n';
+} while (!checkNum(n));
+*/
+if (checkNum(n)) {
+    alert(pow(x, n));
 }
