cafffb77bbab5cbd092da2d9671d55a6a7b0197a
Andrey Dorokhov <charizard2991@yandex.ru>
2019-05-15 18:19:34 +0300

    module-2 and modull-3 full

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,65 @@
  * @returns {Object}
  */
 function find (obj, value) {
+
+    let massOfKeys = [];
+    let currentPath = '';
+    let recurseResult = '';
+
+    for (let property in obj) {
+        if (typeof obj[property] === 'object') {
+            if (!Array.isArray(obj[property])) {
+                currentPath = currentPath + property;
+
+                recurseResult = find(obj[property], value);
+                if (recurseResult !== null) {
+                    currentPath = currentPath + '.';
+                    if (!Array.isArray(recurseResult)) {
+                        massOfKeys.push(currentPath + recurseResult)
+                    } else {
+                        for (let i = 0; i < recurseResult.length; i++) {
+                            massOfKeys.push(currentPath + recurseResult[i]);
+                        }
+                    }
+
+                }
+            }
+        } else {
+            if (obj[property] === value) {
+                massOfKeys.push(property);
+            }
+        }
+        currentPath = '';
+    }
+
+    if (massOfKeys.length === 0) {
+        return null;
+    } else if (massOfKeys.length === 1) {
+        return massOfKeys[0];
+    } else {
+        return massOfKeys;
+    }
+
+
 }
+
+let obj = {
+    A:1,
+    B:2,
+    C: {
+        D:1,
+        E:[3,4,5,1,6,7,8],
+        F: {
+            G:1,
+            H:2
+        },
+        I: {
+            J:1
+        }
+    },
+    K: {
+        L:1,
+        M:1
+    }
+};
+console.log(find(obj, 1));
