1f4161be053abc908cef68475a5fe26e38af5439
Andrey Dorokhov <charizard2991@yandex.ru>
2019-05-20 16:35:58 +0300

    freakin DOM finally started! Module-4 ready

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+    let ul = document.createElement('ul')
+    let list = '';
+    let point;
+    for (let i = 0; i < friends.length; i++) {
+        point = document.createElement('li');
+        point.innerHTML = '' + friends[i].firstName + ' ' + friends[i].lastName;
+        ul.appendChild(point)
+    }
+    return ul;
 }
