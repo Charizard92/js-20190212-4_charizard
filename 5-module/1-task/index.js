3911bc32c35a5c2b5d6ee23f1c631b240089496f
Andrey Dorokhov <charizard2991@yandex.ru>
2019-05-21 13:08:55 +0300

    module-5 ready

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,25 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let rows = table.querySelector('tbody').querySelectorAll('tr');
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].innerHTML.indexOf('data-available') !== -1) {
+            if (rows[i].innerHTML.indexOf('data-available="true"') !== -1) {
+                rows[i].classList.add('available');
+            } else {
+                rows[i].classList.add('unavailable');
+            }
+        } else {
+            rows[i].setAttribute('hidden', true);
+        }
+        let cells = rows[i].querySelectorAll('td');
+        if (cells[2].innerText.indexOf('m') !== -1) {
+            rows[i].classList.add('male');
+        } else {
+            rows[i].classList.add('female');
+        }
+        if (+(cells[1].innerText) < 18) {
+            rows[i].style.textDecoration = 'line-through';
+        }
+    }
 }
